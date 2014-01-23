dea70fb08e673182642f762d6ced9f6d29bf92bf
Tylor Reynolds <tylor.reynolds@agencyrx.com>
2014-01-23 11:39:59 -0500

    change seed to checksum, add fixed modulo, fix decrypt tuples len

--- bubble_babble.js
+++ bubble_babble.js
@@ -19,7 +19,7 @@ var encrypt = function(input, encoding) {
   }
 
   var result = 'x',
-      seed = 1,
+      checksum = 1,
       len = input.length,
       byte1, byte2,
       d, e, i;
@@ -27,7 +27,7 @@ var encrypt = function(input, encoding) {
   // create full tuples
   for (i = 0; i + 1 < len; i += 2) {
     byte1 = input.readInt8(i);
-    result += odd_partial(byte1, seed);
+    result += odd_partial(byte1, checksum);
 
     byte2 = input.readInt8(i + 1);
     d = (byte2 >> 4) & 15;
@@ -35,42 +35,42 @@ var encrypt = function(input, encoding) {
 
     result += consonants.charAt(d) + '-' + consonants.charAt(e);
 
-    seed = ((seed * 5) + (byte1 * 7) + byte2) % 36;
+    checksum = next_checksum(checksum, byte1, byte2);
   }
 
   // handle partial tuple
   if (i < len) {
     byte1 = input.readInt8(i);
-    result += odd_partial(byte1, seed);
+    result += odd_partial(byte1, checksum);
   } else {
-    result += even_partial(seed);
+    result += even_partial(checksum);
   }
 
   result += 'x';
   return result;
 };
 
-var odd_partial = function(raw_byte, seed) {
-  var a = (((raw_byte >> 6) & 3) + seed) % 6,
+var odd_partial = function(raw_byte, checksum) {
+  var a = (((raw_byte >> 6) & 3) + checksum) % 6,
       b = (raw_byte >> 2) & 15,
-      c = ((raw_byte & 3) + Math.floor(seed / 6)) % 6;
+      c = ((raw_byte & 3) + Math.floor(checksum / 6)) % 6;
 
   return vowels.charAt(a) + consonants.charAt(b) + vowels.charAt(c);
 }
 
-var even_partial = function(seed) {
-  var a = Math.floor(seed % 6),
+var even_partial = function(checksum) {
+  var a = checksum % 6,
       b = 16,
-      c = Math.floor(seed / 6);
+      c = Math.floor(checksum / 6);
 
   return vowels.charAt(a) + consonants.charAt(b) + vowels.charAt(c);
 };
 
 var decrypt = function(input) {
   var tuples = input.match(tuple_re),
-      len = tuples.length,
+      len = tuples ? tuples.length : 0,
       char_codes = new Buffer(len * 2),
-      seed = 1,
+      checksum = 1,
       byte1, byte2,
       a_bits, b_bits,
       c_bits, d_bits,
@@ -80,12 +80,12 @@ var decrypt = function(input) {
   for (i = 0; i < len; ++i) {
     tuple = tuples[i];
 
-    a_bits = (Math.max(vowel_map[tuple.charAt(0)] - seed, 0) & 3) << 6;
+    a_bits = (Math.max(vowel_map[tuple.charAt(0)] - checksum, 0) & 3) << 6;
     b_bits = ((consonant_map[tuple.charAt(1)] & 15)) << 2;
-    c_bits = Math.max(vowel_map[tuple.charAt(2)] - Math.floor(seed / 6), 0) & 3;
+    c_bits = Math.max(vowel_map[tuple.charAt(2)] - Math.floor(checksum / 6), 0) & 3;
 
-    if ((a_bits - seed) % 6 >= 4 ||
-        (c_bits - seed) % 6 >= 4) {
+    if (mod(a_bits - checksum, 6) >= 4 ||
+        mod(c_bits - checksum, 6) >= 4) {
       return null;
     }
 
@@ -95,7 +95,7 @@ var decrypt = function(input) {
     byte1 = a_bits | b_bits | c_bits;
     byte2 = d_bits | e_bits;
 
-    seed = ((seed * 5) + (byte1 * 7) + byte2) % 36;
+    checksum = next_checksum(checksum, byte1, byte2);
     char_codes[i * 2] = byte1;
     char_codes[(i * 2) + 1] = byte2;
   }
@@ -103,7 +103,15 @@ var decrypt = function(input) {
   return char_codes;
 };
 
-var input = 'xesuf-disof-gytuf-katof-movif-baxux';
+var next_checksum = function(checksum, byte1, byte2) {
+  return ((checksum * 5) + (byte1 * 7) + byte2) % 36;
+}
+
+var mod = function(n, m) {
+  return ((m % n) + n) % n;
+}
+
+var input = 'xesef-disof-gytuf-katof-movif-baxux';
 
 var decrypted = decrypt(input);
 
